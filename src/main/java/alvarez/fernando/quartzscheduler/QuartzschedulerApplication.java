5a8eea690db7094a703cbd60d5acbb18c1a253b5
Fernando Alvarez <fernando44ca@gmail.com>
2021-07-04 20:43:04 -0300

    Initial files by Spring Initializr

---
+++ src/main/java/alvarez/fernando/quartzscheduler/QuartzschedulerApplication.java
@@ -0,0 +1,13 @@
+package alvarez.fernando.quartzscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuartzschedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuartzschedulerApplication.class, args);
+	}
+
+}
