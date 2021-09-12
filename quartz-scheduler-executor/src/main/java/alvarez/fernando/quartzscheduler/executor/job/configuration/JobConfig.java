ae76a97056fbf8e94bbc5a619835a622dd866e0b
Fernando Alvarez <fernando44ca@gmail.com>
2021-09-12 14:14:18 -0300

    Mode details for configured/scheduled jobs log

--- quartz-scheduler-executor/src/main/java/alvarez/fernando/quartzscheduler/executor/job/configuration/JobConfig.java
+++ quartz-scheduler-executor/src/main/java/alvarez/fernando/quartzscheduler/executor/job/configuration/JobConfig.java
@@ -1,19 +1,14 @@
 package alvarez.fernando.quartzscheduler.executor.job.configuration;
 
 import lombok.extern.slf4j.Slf4j;
-import org.quartz.JobDetail;
-import org.quartz.Scheduler;
-import org.quartz.SchedulerException;
-import org.quartz.Trigger;
+import org.quartz.*;
 import org.springframework.beans.BeanInstantiationException;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.StringUtils;
 
-import java.util.Collection;
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 @Slf4j
 @Configuration
@@ -40,6 +35,23 @@ public class JobConfig implements ApplicationListener<ApplicationReadyEvent> {
 			throw new BeanInstantiationException(this.getClass(), "Error while scheduling jobs", e);
 		}
 		log.info("{} Jobs scheduled", this.jobs.size());
+		
+		for (Map.Entry<JobDetail, Set<? extends Trigger>> entry : this.jobs.entrySet()) {
+			final JobDetail jobDetail = entry.getKey();
+			final Set<? extends Trigger> triggers = entry.getValue();
+			
+			final List<String> triggerAsString = new ArrayList<>(triggers.size());
+			for (Trigger trigger : triggers) {
+				if (trigger instanceof CronTrigger) {
+					triggerAsString.add(((CronTrigger) trigger).getCronExpression());
+				} else {
+					triggerAsString.add(trigger.toString());
+				}
+			}
+			
+			log.info("Job {} scheduled for \"{}\": \"{}\"", jobDetail.getJobClass(), StringUtils.collectionToCommaDelimitedString(triggerAsString), jobDetail.getDescription());
+		}
+		
 	}
 	
 }
